43dbbd8a2b106224cd949f2abe677f5caf66fd9c
nagas <nagas@DESKTOP-5EJH7KC>
2020-03-27 12:54:44 +0530

    Login Feature Commit

---
+++ day1-demo/src/main/java/org/cap/model/LoginFeature.java
@@ -0,0 +1,24 @@
+package org.cap.model;
+
+public class LoginFeature {
+         public int login_id;
+         public int report_id;
+		public int getLogin_id() {
+			return login_id;
+		}
+		public void setLogin_id(int login_id) {
+			this.login_id = login_id;
+		}
+		public int getReport_id() {
+			return report_id;
+		}
+		public void setReport_id(int report_id) {
+			this.report_id = report_id;
+		}
+		public LoginFeature(int login_id, int report_id) {
+			super();
+			this.login_id = login_id;
+			this.report_id = report_id;
+		}
+         
+}
